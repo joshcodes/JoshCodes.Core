3708a3efbc474ff45b50dca0ff21568548477fb5
Josh <josh@magicmoments.com>
2013-08-20 16:37:47 -0400

    added extension to download urls

---
+++ Net/GenericExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace JoshCodes.Net
+{
+    public static class GenericExtensions
+    {
+        public static IEnumerable<KeyValuePair<string, Stream>> ToStream(this IEnumerable<KeyValuePair<string, Uri>> uriKvps)
+        {
+            foreach (var uriKvp in uriKvps) 
+            {
+                var key = uriKvp.Key;
+                var uri = uriKvp.Value;
+                yield return new KeyValuePair<string, System.IO.Stream>(key, uri.Download());
+            }
+        }
+
+        public static Stream Download(this Uri url)
+        {
+            using (var webClient = new System.Net.WebClient())
+            {
+                var data = webClient.DownloadData(url);
+                return new MemoryStream(data);
+            }
+        }
+    }
+}
