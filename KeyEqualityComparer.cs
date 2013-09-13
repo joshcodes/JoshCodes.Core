4c3241cf3617f094d3a6753c0388f1f3e9c11216
Josh <josh@magicmoments.com>
2013-09-13 18:41:32 -0400

    added generic code for comparisions found on SO

---
+++ KeyEqualityComparer.cs
@@ -0,0 +1,49 @@
+﻿// From http://stackoverflow.com/questions/98033/wrap-a-delegate-in-an-iequalitycomparer
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JoshCodes.Core
+{
+    
+    public static class Equality<T>
+    {
+        public static IEqualityComparer<T> CreateComparer<V>(Func<T, V> keySelector)
+        {
+            return CreateComparer(keySelector, null);
+        }
+
+        public static IEqualityComparer<T> CreateComparer<V>(Func<T, V> keySelector,
+                                                             IEqualityComparer<V> comparer)
+        {
+            return new KeyEqualityComparer<V>(keySelector, comparer);
+        }
+
+        class KeyEqualityComparer<V> : IEqualityComparer<T>
+        {
+            readonly Func<T, V> keySelector;
+            readonly IEqualityComparer<V> comparer;
+
+            public KeyEqualityComparer(Func<T, V> keySelector,
+                                       IEqualityComparer<V> comparer)
+            {
+                if (keySelector == null)
+                    throw new ArgumentNullException("keySelector");
+
+                this.keySelector = keySelector;
+                this.comparer = comparer ?? EqualityComparer<V>.Default;
+            }
+
+            public bool Equals(T x, T y)
+            {
+                return comparer.Equals(keySelector(x), keySelector(y));
+            }
+
+            public int GetHashCode(T obj)
+            {
+                return comparer.GetHashCode(keySelector(obj));
+            }
+        }
+    }
+}
