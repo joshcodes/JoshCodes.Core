3083e61f41bc7bf6c2a45d2b0ac07e5195b82145
Josh <josh@magicmoments.com>
2013-10-31 16:26:34 -0400

    added ability to create secure guids

---
+++ SecureGuid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JoshCodes.Core
+{
+    public class SecureGuid
+    {
+        public static Guid Generate()
+        {
+            var guidData = new byte[0x10];
+            System.Security.Cryptography.RandomNumberGenerator.Create().GetBytes(guidData);
+            return new Guid(guidData);
+        }
+    }
+}
