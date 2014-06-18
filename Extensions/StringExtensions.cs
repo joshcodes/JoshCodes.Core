cc04d8cc832bc92ccedd3cd363c5401fc32f8446
joshcodes <josh.wingstrom@lonelyplanet.com>
2014-06-18 16:13:55 -0500

    added ToStream to string method as an extension

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JoshCodes.Core
+{
+    public static class StringExtensions
+    {
+        public static Stream ToStream(this string str)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(str);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
